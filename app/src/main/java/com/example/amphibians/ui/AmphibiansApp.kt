21d94b2391cbf0f1b4127d3114f04993646e7d32
achroma <alinachroma@gmail.com>
2024-05-17 19:47:30 +0200

    cleaned up MainActivity
    Removed Preview from the MainActivity, moved it to a separate class for the AmphibiansApp instead - main important composables are supposed to be there.

---
+++ app/src/main/java/com/example/amphibians/ui/AmphibiansApp.kt
@@ -0,0 +1,18 @@
+package com.example.amphibians.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.amphibians.ui.theme.AmphibiansTheme
+
+@Composable
+fun AmphibiansApp() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AmphibiansAppPreview() {
+    AmphibiansTheme {
+        AmphibiansApp()
+    }
+}
