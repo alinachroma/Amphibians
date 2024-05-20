23393038179e122e8673b45a284184b9d8a34982
achroma <alinachroma@gmail.com>
2024-05-20 19:56:35 +0200

    basic UI - composables and previews

--- app/src/main/java/com/example/amphibians/ui/AmphibiansApp.kt
+++ app/src/main/java/com/example/amphibians/ui/AmphibiansApp.kt
@@ -1,15 +1,79 @@
 package com.example.amphibians.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.amphibians.ui.theme.AmphibiansTheme
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 
 @Composable
 fun AmphibiansApp(
-    viewModel: AmphibiansViewModel = viewModel()
+    viewModel: AmphibiansViewModel = viewModel(),
+    modifier: Modifier = Modifier
 ) {
+    Scaffold(
+        topBar = { TopBar(topAppBarTitle = "") },
+        modifier = modifier,
+        content = { paddingValues ->
+            HomeScreen(modifier = Modifier.padding(paddingValues))
+        }
+    )
+}
 
+@Composable
+fun TopBar(
+    topAppBarTitle: String,
+    modifier: Modifier = Modifier
+) {
+    Text(text = topAppBarTitle)
+}
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+    AmphibianCardsList()
+}
+
+@Composable
+fun AmphibianCardsList(
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(
+        modifier = modifier,
+        contentPadding = PaddingValues(5.dp),
+    ) {
+    }
+}
+
+@Composable
+fun AmphibianCard(
+    modifier: Modifier = Modifier
+) {
+    Column {
+        Text(text = "amphibianName")
+        AsyncImage(
+            model = ImageRequest.Builder(context = LocalContext.current).build(),
+            contentDescription = "",
+            contentScale = ContentScale.Crop,
+            modifier = modifier.fillMaxWidth()
+        )
+        Text(text = "text")
+    }
 }
 
 @Preview(showBackground = true)
@@ -17,5 +81,21 @@ fun AmphibiansApp(
 fun AmphibiansAppPreview() {
     AmphibiansTheme {
         AmphibiansApp()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AmphibianCardPreview() {
+    AmphibiansTheme {
+        AmphibianCard()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AmphibianCardsListPreview() {
+    AmphibiansTheme {
+        AmphibianCardsList()
     }
 }
