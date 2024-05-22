7ab0ae83365224b8e8215418b8731b8f2aa514bc
achroma <alinachroma@gmail.com>
2024-05-22 20:21:56 +0200

    implemented a function that calls the web service

--- app/src/main/java/com/example/amphibians/ui/AmphibiansViewModel.kt
+++ app/src/main/java/com/example/amphibians/ui/AmphibiansViewModel.kt
@@ -4,7 +4,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.amphibians.data.Amphibian
+import com.example.amphibians.network.AmphibiansApi
+import kotlinx.coroutines.launch
+
 sealed class AmphibiansUiState {
     data class Success(val amphibiansList: List<Amphibian>) : AmphibiansUiState()
     object Loading : AmphibiansUiState()
@@ -14,3 +18,13 @@ sealed class AmphibiansUiState {
 class AmphibiansViewModel : ViewModel() {
     var uiState: AmphibiansUiState by mutableStateOf(AmphibiansUiState.Loading)
         private set
+
+    private fun getAmphibiansData() {
+        viewModelScope.launch {
+            val result = AmphibiansApi.retrofitService.getAmphibiansData()
+            uiState = AmphibiansUiState.Loading
+        }
+    }
+
+
+}
