a0569ed84659e4bdf42c908f3d288c0282f84e95
achroma <alinachroma@gmail.com>
2024-05-28 19:37:21 +0200

    created an AppContainer
    and removed a baseUrl, retrofit and retrofit service there to work with the repository that will be provided from there

--- app/src/main/java/com/example/amphibians/network/AmphibiansApiService.kt
+++ app/src/main/java/com/example/amphibians/network/AmphibiansApiService.kt
@@ -7,19 +7,7 @@ import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFact
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 
-private const val BASE_URL = "https://android-kotlin-fun-mars-server.appspot.com/"
-
-private val retrofit = Retrofit.Builder()
-    .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
-    .baseUrl(BASE_URL)
-    .build()
 interface AmphibiansApiService {
     @GET("amphibians")
     suspend fun getAmphibiansData() : List<Amphibian>
-}
-
-object AmphibiansApi {
-    val retrofitService: AmphibiansApiService by lazy {
-        retrofit.create(AmphibiansApiService::class.java)
-    }
 }
