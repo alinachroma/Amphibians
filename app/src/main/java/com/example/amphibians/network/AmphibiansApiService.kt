4cd72a2a5d19ad7f49b10cd6f1384b2baf3b5f7c
achroma <alinachroma@gmail.com>
2024-05-28 15:47:47 +0200

    use serialization instead of scalar factory
    Kotlin serialization library lets us get a runtime object from the JSON we’re fetching from the web server

--- app/src/main/java/com/example/amphibians/network/AmphibiansApiService.kt
+++ app/src/main/java/com/example/amphibians/network/AmphibiansApiService.kt
@@ -2,13 +2,15 @@ package com.example.amphibians.network
 
 import com.example.amphibians.data.Amphibian
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
 
 private const val BASE_URL = "https://android-kotlin-fun-mars-server.appspot.com/"
 
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
     .baseUrl(BASE_URL)
     .build()
 interface AmphibiansApiService {
