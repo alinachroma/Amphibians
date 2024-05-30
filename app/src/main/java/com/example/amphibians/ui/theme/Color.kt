efcd2fa605ad9cd171de994ed67b79f427e51f98
achroma <alinachroma@gmail.com>
2024-05-30 18:07:10 +0200

    added colors according to the references

--- app/src/main/java/com/example/amphibians/ui/theme/Color.kt
+++ app/src/main/java/com/example/amphibians/ui/theme/Color.kt
@@ -2,10 +2,13 @@ package com.example.amphibians.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val background_light = Color(0xFFFCFDF6)
+val surface_light = Color(0xFFDEE5D8)
+val text_light = Color.DarkGray
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+
+val background_dark = Color(0xFF1A1C19)
+val surface_dark = Color(0xFF424940)
+val text_dark = Color.LightGray
+
+
