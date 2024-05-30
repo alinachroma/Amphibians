275721a32c101c1c9ded8dcd56f11be99ab59d5b
achroma <alinachroma@gmail.com>
2024-05-30 20:07:29 +0200

    wired the top bar

--- app/src/main/java/com/example/amphibians/ui/AmphibiansApp.kt
+++ app/src/main/java/com/example/amphibians/ui/AmphibiansApp.kt
@@ -6,16 +6,27 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.example.amphibians.R
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.amphibians.data.Amphibian
 import com.example.amphibians.ui.AmphibiansUiState.Success
+import com.example.amphibians.ui.items.AmphibiansTopBar
 import com.example.amphibians.ui.screens.HomeScreen
 import com.example.amphibians.ui.theme.AmphibiansTheme
 
 @Composable
 fun AmphibiansApp() {
-    Scaffold {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            AmphibiansTopBar(
+                topAppBarTitle = stringResource(id = R.string.app_name),
+                modifier = Modifier
+            )
+        }
+    ) {
         Surface(
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
