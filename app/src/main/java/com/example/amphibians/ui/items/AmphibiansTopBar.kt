07830af7ef9aa94966d58e4da47ef4a7f5b57de5
achroma <alinachroma@gmail.com>
2024-05-31 11:44:00 +0200

    added top bar color

--- app/src/main/java/com/example/amphibians/ui/items/AmphibiansTopBar.kt
+++ app/src/main/java/com/example/amphibians/ui/items/AmphibiansTopBar.kt
@@ -1,15 +1,14 @@
 package com.example.amphibians.ui.items
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
-import com.example.amphibians.data.Amphibian
 import com.example.amphibians.R
 import com.example.amphibians.ui.theme.AmphibiansTheme
 import com.example.amphibians.utils.ThemePreviews
@@ -21,6 +20,7 @@ fun AmphibiansTopBar(
     modifier: Modifier = Modifier
 ) {
     TopAppBar(
+        colors = TopAppBarDefaults.topAppBarColors(MaterialTheme.colorScheme.background),
         title = {
             Text(
                 text = topAppBarTitle,
@@ -29,7 +29,6 @@ fun AmphibiansTopBar(
                     .padding(
                         start = dimensionResource(id = R.dimen.padding_medium),
                         end = dimensionResource(id = R.dimen.padding_medium))
-                    .background(color = MaterialTheme.colorScheme.background)
             )
         }
     )
